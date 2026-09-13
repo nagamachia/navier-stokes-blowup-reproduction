bacfac96ba46acce2f3c86ddc094aef8074756bb
A.Nagamachi <55824245+nagamachia@users.noreply.github.com>
2026-09-13 09:52:43 +0900

    test: compile A.7 full schedule bridge

---
+++ tests/appendix_a7_full_schedule_compile.cpp
@@ -0,0 +1,5 @@
+#include "appendix_a7_full_schedule.hpp"
+void appendix_a7_full_schedule_compile_check() {
+    auto q = nsblowup::appendix_a7_heat_tail_discrepancy_normalized(0.0, 0.08, 120.0, 8.0, 128);
+    (void)q;
+}
